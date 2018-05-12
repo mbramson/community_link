7479abdcacb82185b46a6705c0e67f0e1a01a2ff
Bobby McDonald <bmcdonald@covermymeds.com>
2018-05-12 18:12:52 -0400

    Fix failing spec

--- test/community_link_web/controllers/page_controller_test.exs
+++ test/community_link_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule CommunityLinkWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Social Network"
   end
 end
